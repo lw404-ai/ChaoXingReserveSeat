2891d0de6fec64eb433066d722e6d5a6cf2aa512
LW404 <liwei-@outlook.com>
2024-08-14 12:11:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,11 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-get_current_time = lambda action: time.strftime("%H:%M:%S", time.localtime(time.time() + 8*3600)) if action else time.strftime("%H:%M:%S", time.localtime(time.time()))
-get_current_dayofweek = lambda action: time.strftime("%A", time.localtime(time.time() + 8*3600)) if action else time.strftime("%A", time.localtime(time.time()))
+# get_current_time = lambda action: time.strftime("%H:%M:%S", time.localtime(time.time() + 8*3600)) if action else time.strftime("%H:%M:%S", time.localtime(time.time()))
+# get_current_dayofweek = lambda action: time.strftime("%A", time.localtime(time.time() + 8*3600)) if action else time.strftime("%A", time.localtime(time.time()))
+
+get_current_time = lambda action: time.strftime("%H:%M:%S", time.localtime(time.time() + 16*3600 - 15*60)) if action else time.strftime("%H:%M:%S", time.localtime(time.time()))
+get_current_dayofweek = lambda action: time.strftime("%A", time.localtime(time.time() + 16*3600 - 15*60)) if action else time.strftime("%A", time.localtime(time.time()))
 
 
 SLEEPTIME = 0.2 # 每次抢座的间隔
